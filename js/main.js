b2c8327a0d85b1462310361c703a9f2c36c793da
p22fd <80908612+p22fd@users.noreply.github.com>
2026-06-27 09:49:54 -0500

    Initial commit - sitio web portafolio Isa y Pao

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('¡Gracias por contactarnos! Te responderemos pronto.');
+  this.reset();
+});
